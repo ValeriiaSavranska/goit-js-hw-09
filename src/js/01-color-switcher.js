f583a8e7c516f0d4b45c3cb53e3d2e0f6f01bf6b
ValeriiaSavranska <lerarog1997@gmail.com>
2021-11-17 19:36:42 +0200

    JS Домашнє завдання №9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startClick = () => {
+  startBtnRef.setAttribute('disabled', true);
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const stopClick = () => {
+  startBtnRef.removeAttribute('disabled');
+  clearInterval(timerId);
+};
+
+startBtnRef.addEventListener('click', startClick);
+stopBtnRef.addEventListener('click', stopClick);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+let delayInp = null;
+let stepInp = null;
+let amountInp = null;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay }); // Fulfill
+      } else {
+        reject({ position, delay }); // Reject
+      }
+    }, delay);
+  });
 }
+
+const submitHandler = e => {
+  e.preventDefault();
+  if (!e.target.tagName === 'BUTTON') return;
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  // if (delay.value === null || step.value === null || amount.value === null) {
+  //   return;
+  // }
+
+  delayInp = Number(delay.value);
+  stepInp = Number(step.value);
+  amountInp = Number(amount.value);
+
+  for (let i = 1; i <= amountInp; i++) {
+    createPromise(i, delayInp)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayInp += stepInp;
+  }
+
+  e.currentTarget.reset();
+};
+formRef.addEventListener('submit', submitHandler);
